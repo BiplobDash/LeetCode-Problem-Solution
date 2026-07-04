6116e50711ad8cb7f7ef0436306bb874eba7c6e6
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2026-07-04 10:25:29 +0600

    Koko Eating Bananas Problem Solution

---
+++ Koko Eating Bananas.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    bool isOk(vector<int>& piles, int mid, int h){
+        int sum = 0;
+        for(auto p : piles){
+            sum += (p + mid - 1) / mid;
+        }
+        return (sum > h);
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int mx = *max_element(piles.begin(), piles.end());
+        int l = 1, r = max(mx, 1000005);
+
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(isOk(piles, mid, h)) l = mid + 1;
+            else r = mid;
+        }
+
+        return l;
+    }
+};
